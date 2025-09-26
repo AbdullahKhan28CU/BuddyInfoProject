decd8ef1bb67002c409794769c6e7c504fed94a0
Abdullah Khan <abkhan0517@gmail.com>
2025-09-26 12:14:50 -0400

    Updated add function

---
+++ src/AddressBook.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> Buddies;
+
+    public AddressBook() {
+        Buddies = new ArrayList<BuddyInfo>();
+    }
+
+    public void addBuddy(BuddyInfo buddy) {
+        if (buddy != null) {
+            Buddies.add(buddy);
+        }
+    }
+
+    public void removeBuddy(int buddyNum) {
+        Buddies.remove(buddyNum);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address book");
+        AddressBook Book = new AddressBook();
+        BuddyInfo buddy = new BuddyInfo();
+        Book.addBuddy (buddy);
+        Book.removeBuddy(0);
+    }
+}
